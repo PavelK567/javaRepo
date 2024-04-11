a86ae2fde7ef84b40739a3af7e40748a452477c8
PavelK567 <159823643+PavelK567@users.noreply.github.com>
2024-04-11 13:25:21 -0400

    Updated

---
+++ src/javaRepo.java
@@ -0,0 +1,28 @@
+
+
+public class javaRepo {
+    
+    // class level vars
+    
+    public static void main(String[] args) throws Exception {
+
+        // datatypes varName [=value];
+        int games = 3;
+        double allowance = 40.50;
+        long battlePass; // We define battlepass 
+        
+        battlePass = 15; // We assign a value to battlepass
+
+        // Print out our var values along with a string that explains our values
+        System.out.println("Number of games I own : " + games);
+        System.out.println("My monthly allowance : " + allowance);
+        System.out.println("Cost of a single battlepass : " + battlePass);
+
+        // With our current Allowance, how many battle passes will we be able to buy?
+        System.out.println("Amount of battle passes we can buy : " + Math.floor(allowance/battlePass));
+
+        // What is the total cost of battle passes
+        System.out.println("Total cost of battle passes : " + (battlePass * games));
+    }   
+
+}
